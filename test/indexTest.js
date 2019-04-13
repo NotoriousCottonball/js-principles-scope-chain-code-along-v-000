ebda170052bfcef495fa0f8bb3ec7e72d1704626
George  Pianka <gpianka1@gmail.com>
2019-04-13 06:25:39 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,7 +4,7 @@ describe('Fix the Scope', function() {
   describe('attemptTwoFavoriteCustomers', function(){
     it('unsuccessfully tries to declare favoriteCustomer with let two times', () => {
          try {
-           attemptTwoFavoriteCustomers()
+           window.attemptTwoFavoriteCustomers()
          } catch (error) {
          var ErrorMessage = error.name
          expect(ErrorMessage).to.equal('SyntaxError')
