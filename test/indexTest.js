954bc4ea75ef7c9df1f85b0379341fc55db122ec
George  Pianka <gpianka1@gmail.com>
2019-04-13 05:32:36 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,7 @@ describe('Fix the Scope', function() {
   
   describe('attemptTwoFavoriteCustomers', function(){
     it('unsuccessfully tries to declare favoriteCustomer with let two times', () => {
-      expect(attemptTwoFavoriteCustomers().to.throw('SyntaxError'))
+      expect(this.to.throw('SyntaxError'))
     })
     
   describe('declare customerName to be bob in global scope', function() {
