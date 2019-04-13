204439ba926d8fcb9f498444360d0004d430b7d8
George  Pianka <gpianka1@gmail.com>
2019-04-13 06:23:41 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,7 +4,7 @@ describe('Fix the Scope', function() {
   describe('attemptTwoFavoriteCustomers', function(){
     it('unsuccessfully tries to declare favoriteCustomer with let two times', () => {
          try {
-     attemptTwoFavoriteCustomers()
+           attemptTwoFavoriteCustomers()
         } catch (error) {
         var ErrorMessage = error
         
