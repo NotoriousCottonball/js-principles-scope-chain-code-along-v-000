6b79f28b59dfea4683c47b29a0d7eb248b745a25
George  Pianka <gpianka1@gmail.com>
2019-04-13 05:07:02 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -15,7 +15,7 @@ describe('Fix the Scope', function() {
   })
   describe('setBestCustomer()', function() {
     it('setBestCustomer', () => {
-      expect(window.bestCustomer).to.equal(undefined)
+      expect(window.bestCustomer).to.equal(3)
       window.setBestCustomer()
       expect(window.bestCustomer).to.equal('not bob')
     })
